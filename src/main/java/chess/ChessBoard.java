bf61594f9e20be1b73f0e6bbe96d864a92a40f1a
unknown <matthewweiss@bellsouth.net>
2025-09-29 23:48:10 -0400

    Created initial methods for ChessBoard, implemented constructor and getBoard for implementation

--- src/main/java/chess/ChessBoard.java
+++ src/main/java/chess/ChessBoard.java
@@ -4,4 +4,24 @@ package chess;
  * Interface dictating methods for chess board implementation.
  */
 public interface ChessBoard {
+
+  /**
+   * Initializes chess game -- sets up pieces in correct places.
+   */
+  void buildBoard();
+
+  /**
+   * Returns visual representation of the chess board.
+   *
+   * @return String of board to print to player(s)
+   */
+  String visualizeBoard();
+
+  /**
+   * Returns chess board as data structure.
+   *
+   * @return 2d String array of the board
+   */
+  String[][] getBoard();
+
 }
--- src/main/java/chess/ChessBoardImpl.java
+++ src/main/java/chess/ChessBoardImpl.java
@@ -4,4 +4,29 @@ package chess;
  * Implementation for ChessBoard.
  */
 public class ChessBoardImpl implements ChessBoard {
+
+  private final String[][] board;
+
+  /**
+   * Constructor for chess board implementation.
+   * Sets dimensions of chess board.
+   */
+  public ChessBoardImpl() {
+    board = new String[8][8];
+  }
+
+  @Override
+  public void buildBoard() {
+    System.out.println("Building Board");
+  }
+
+  @Override
+  public String visualizeBoard() {
+    return "";
+  }
+
+  @Override
+  public String[][] getBoard() {
+    return this.board;
+  }
 }
